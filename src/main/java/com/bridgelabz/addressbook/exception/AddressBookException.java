3f77808a8dffd22b3995750b04207842ff273e39
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-12-17 02:14:55 +0530

    implement validation exception

---
+++ src/main/java/com/bridgelabz/addressbook/exception/AddressBookException.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.addressbook.exception;
+
+public class AddressBookException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -5492382145271539206L;
+
+    public AddressBookException() {
+    }
+
+    public AddressBookException(String message) {
+        super(message);
+    }
+
+}
