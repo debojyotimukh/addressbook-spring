0ef101a582a63fa4774989685912b437473d29c3
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-12-17 02:27:25 +0530

    fields made final

--- src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/ContactDTO.java
@@ -1,33 +1,36 @@
 package com.bridgelabz.addressbook.dto;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import lombok.Data;
 
 public @Data class ContactDTO {
 
+    @NotNull
     @Pattern(regexp = "^[A-Z]{1}[a-z]{3,}", message = "Invalid first name!")
-    private String firstName;
+    private final String firstName;
 
+    @NotNull
     @Pattern(regexp = "^[A-Z]{1}[a-z]{3,}", message = "Invalid first name!")
-    private String lastName;
+    private final String lastName;
 
     @Pattern(regexp = "^[0-9\\s\\,]+[\\w]{3,}.+$", message = "Invalid address!")
-    private String address;
+    private final String address;
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}", message = "Invalid city!")
-    private String city;
+    private final String city;
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}", message = "Invalid state!")
-    private String state;
+    private final String state;
 
     @Pattern(regexp = "^[0-9]{6}$", message = "Invalid zip code!")
-    private String zip;
+    private final String zip;
 
     @Pattern(regexp = "^([+]{1})??([0-9]{2})??([0-9]{10})$", message = "Invalid phone number!")
-    private String phone;
+    private final String phone;
 
     @Pattern(regexp = "^[\\w-+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$", message = "Invalid email address!")
-    private String email;
+    private final String email;
 
 }
